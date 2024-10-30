106eb9586da99d3b1328b6d4be76aea701cea35f
akint <akinthachandinu5@gmail.com>
2024-10-30 21:52:58 +0530

    add

---
+++ js.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $('input').on('click', function () {
+        let buttonId = $(this).attr('id');
+        let buttonText = $(this).val();
+        let display = $('#display');
+
+
+
+        if (buttonId === 'AC'){
+            display.val(null);
+
+        }else if(buttonId === 'DE'){
+            display.val(display.val().slice(0, -1));
+
+        }else if(buttonId === '='){
+            try {
+                display.val(eval(display.val()));
+            } catch (error) {
+                display.val("Error");
+            }
+        }
+
+        else {
+            display.val(display.val() + buttonText);
+        }
+
+
+    });
+});
